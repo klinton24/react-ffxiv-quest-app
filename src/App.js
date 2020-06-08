a5c949f69ec9b262109aecff58483d6c73914776
K Smith <klinton.jp@gmail.com>
2020-06-07 23:23:55 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Form from "./components/Form/Form";
+import Progress from "./components/Progress/Progress";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FFXIV Quest Calculator</h1>
+      <Progress />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <h1>Form Components</h1>
+    </div>
+  );
+};
+
+export default Form;
